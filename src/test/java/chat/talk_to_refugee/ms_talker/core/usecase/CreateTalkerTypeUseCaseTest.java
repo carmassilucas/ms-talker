606ffc09ea67b1a078bc9c65c4c9a0fc0af27216
lucas.carmassi <lucas.carmassi@gruporevise.com.br>
2025-09-24 15:03:07 -0300

    test: testando cadastro de tipo de talker

---
+++ src/test/java/chat/talk_to_refugee/ms_talker/core/usecase/CreateTalkerTypeUseCaseTest.java
@@ -0,0 +1,32 @@
+package chat.talk_to_refugee.ms_talker.core.usecase;
+
+import chat.talk_to_refugee.ms_talker.core.domain.TalkerType;
+import chat.talk_to_refugee.ms_talker.core.port.outbound.TalkerTypeRepositoryAdapterPort;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class CreateTalkerTypeUseCaseTest {
+
+    @InjectMocks
+    private CreateTalkerTypeUseCase create;
+
+    @Mock
+    private TalkerTypeRepositoryAdapterPort repository;
+
+    @Test
+    @DisplayName("Deve cadastrar tipo")
+    void must_register_type() {
+        var type = new TalkerType(1L, "administrator");
+
+        this.create.execute(type);
+
+        verify(this.repository).save(type);
+    }
+}

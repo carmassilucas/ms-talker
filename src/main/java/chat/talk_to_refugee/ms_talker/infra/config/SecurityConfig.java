5c2f6ee25d9a204c13c29b16dc0dc12d0959050d
lucas.carmassi <lucas.carmassi@gruporevise.com.br>
2025-09-25 11:29:08 -0300

    feat: permitindo requisições ao endpoint de criação de talker

---
+++ src/main/java/chat/talk_to_refugee/ms_talker/infra/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package chat.talk_to_refugee.ms_talker.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorization ->
+                        authorization.requestMatchers(HttpMethod.POST, "/talkers").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}

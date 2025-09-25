45e62d2495ac96be04365fb6bff49875f6f13423
lucas.carmassi <lucas.carmassi@gruporevise.com.br>
2025-09-25 11:14:37 -0300

    fix: setando campos instant de talker entity via hibernate

--- src/main/java/chat/talk_to_refugee/ms_talker/adapter/outbound/repository/TalkerEntity.java
+++ src/main/java/chat/talk_to_refugee/ms_talker/adapter/outbound/repository/TalkerEntity.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 import java.time.LocalDate;
@@ -50,9 +52,11 @@ public @Data class TalkerEntity {
     @Column(name = "password", nullable = false)
     private String password;
 
+    @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt;
 
+    @UpdateTimestamp
     @Column(name = "updated_at", nullable = false)
     private Instant updatedAt;
 }

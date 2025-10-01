bb243b1c3ee48399767ccf2910825c86113a17de
lucas.carmassi <lucas.carmassi@gruporevise.com.br>
2025-10-01 08:46:42 -0300

    test: aumentando cobertura de teste na autenticação do talker

--- src/test/java/chat/talk_to_refugee/ms_talker/core/usecase/AuthTalkerUseCaseTest.java
+++ src/test/java/chat/talk_to_refugee/ms_talker/core/usecase/AuthTalkerUseCaseTest.java
@@ -3,6 +3,8 @@ package chat.talk_to_refugee.ms_talker.core.usecase;
 import chat.talk_to_refugee.ms_talker.adapter.inbound.resource.request.AuthTalkerRequest;
 import chat.talk_to_refugee.ms_talker.adapter.inbound.resource.response.AuthTalkerResponse;
 import chat.talk_to_refugee.ms_talker.core.domain.Talker;
+import chat.talk_to_refugee.ms_talker.core.exception.PasswordDoesNotMatchesException;
+import chat.talk_to_refugee.ms_talker.core.exception.TalkerNotFoundException;
 import chat.talk_to_refugee.ms_talker.core.port.outbound.AuthenticatorAdapterPort;
 import chat.talk_to_refugee.ms_talker.core.port.outbound.PasswordEncoderAdapterPort;
 import chat.talk_to_refugee.ms_talker.core.port.outbound.TalkerRepositoryAdapterPort;
@@ -56,5 +58,34 @@ class AuthTalkerUseCaseTest {
         verify(this.repository).findByEmail(requestBody.email());
         verify(this.passwordEncoder).matches(requestBody.password(), talker.getPassword());
         verify(this.authenticator).auth(talker.getId().toString());
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção quando talker não encontrado")
+    void should_throw_exception_when_talker_not_found() {
+        var requestBody = new AuthTalkerRequest("john.doe@t2r.com", "john.doe");
+
+        assertThrows(TalkerNotFoundException.class, () -> this.auth.execute(requestBody));
+
+        verify(this.authenticator, never()).auth(anyString());
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção quando senhas não coincidem")
+    void should_throw_exception_when_passwords_do_not_match() {
+        var requestBody = new AuthTalkerRequest("john.doe@t2r.com", "john.doe");
+
+        var id = UUID.randomUUID();
+
+        var talker = new Talker();
+        talker.setId(id);
+        talker.setPassword("john.doe");
+
+        when(this.repository.findByEmail(requestBody.email())).thenReturn(Optional.of(talker));
+        when(this.passwordEncoder.matches(requestBody.password(), talker.getPassword())).thenReturn(false);
+
+        assertThrows(PasswordDoesNotMatchesException.class, () -> this.auth.execute(requestBody));
+
+        verify(this.authenticator, never()).auth(anyString());
     }
 }
